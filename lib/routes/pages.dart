65558ebf1a1dde3474977dc996c40ea6c6f166dd
yellowtoast <kjh9519@naver.com>
2022-08-22 21:14:13 +0900

    add: add pages and routes

--- lib/routes/pages.dart
+++ lib/routes/pages.dart
@@ -1,6 +1,7 @@
 import 'package:get/get.dart';
 import 'package:kurly_shopping_cart_app/routes/routes.dart';
-
+import 'package:kurly_shopping_cart_app/ui/pages/shopping_cart/shopping_cart_1.dart';
+import 'package:kurly_shopping_cart_app/ui/pages/shopping_cart/shopping_cart_2.dart';
 import '../ui/pages/home/home_page.dart';
 
 class KurlyPages {
@@ -9,5 +10,13 @@ class KurlyPages {
         name: Routes.HOME,
         page: () => HomePage(),
         transition: Transition.noTransition),
+    GetPage(
+        name: Routes.SHOPPING_CART_STEP1,
+        page: () => ShoppingCartStep1(),
+        transition: Transition.cupertino),
+    GetPage(
+        name: Routes.SHOPPING_CART_STEP2,
+        page: () => ShoppingCartStep2(),
+        transition: Transition.cupertino),
   ];
 }
--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -4,6 +4,8 @@ abstract class Routes {
   static const SPLASH = '/';
   static const START = '/start';
   static const HOME = '/home';
+  static const SHOPPING_CART_STEP1 = '/shopping/1';
+  static const SHOPPING_CART_STEP2 = '/shopping/2';
   static const SIGNUP_PW = '/signup/name_pw';
   static const SIGNUP_PROFILE = '/signup/profile';
   static const SIGNUP_CONIMAL_STEP1 = '/signup/conimal/1';
