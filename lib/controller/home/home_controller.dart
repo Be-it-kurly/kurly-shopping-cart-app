9fade094e4e94b3394903deb546e48786b0e0eb3
yellowtoast <kjh9519@naver.com>
2022-08-22 21:13:54 +0900

    feat: implement home controller

--- lib/controller/home/home_controller.dart
+++ lib/controller/home/home_controller.dart
@@ -1,3 +1,38 @@
 import 'package:get/get.dart';
 
-class HomeController extends GetxController {}
+enum PeopleAmount { one, two, three, four }
+
+String peopleAmountToString(PeopleAmount peopleAmount) {
+  switch (peopleAmount) {
+    case PeopleAmount.one:
+      return '1인';
+    case PeopleAmount.two:
+      return '2인';
+    case PeopleAmount.three:
+      return '3인';
+    case PeopleAmount.four:
+      return '4인';
+    default:
+      return '';
+  }
+}
+
+class HomeController extends GetxController {
+  Rxn<PeopleAmount> selectedPeopleAmount = Rxn<PeopleAmount>();
+  List<PeopleAmount> peopleAmountList = [
+    PeopleAmount.one,
+    PeopleAmount.two,
+    PeopleAmount.three,
+    PeopleAmount.four
+  ];
+
+  @override
+  void onInit() {
+    super.onInit();
+  }
+
+  @override
+  void onReady() {
+    super.onReady();
+  }
+}
